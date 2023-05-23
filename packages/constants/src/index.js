9f8010659e65b9f7ec3ee82fcdb93fc6874834d4
iskdrews <36774944+iskdrews@users.noreply.github.com>
2023-05-23 20:50:58 +0200

    feat: (wip) finish @cryptkeeper/constants package

---
+++ packages/constants/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DEFAULT_ROUND = exports.ZERO_ADDRESS = void 0;
+exports.ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+exports.DEFAULT_ROUND = 4;
+//# sourceMappingURL=index.js.map
