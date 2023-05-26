3446f74ea48379ab336d1658118861d791463b57
iskdrews <36774944+iskdrews@users.noreply.github.com>
2023-05-26 11:10:53 +0200

    feat: (wip) @cryptkeeper/constants package

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -4,14 +4,14 @@ export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const DEFAULT_ROUND = 4;
 
 export const WEB2_PROVIDER_OPTIONS: readonly SelectOption[] = [
-    { value: "twitter", label: "Twitter", icon: ["fab", "twitter"] },
-    { value: "reddit", label: "Reddit", icon: ["fab", "reddit"] },
-    { value: "github", label: "Github", icon: ["fab", "github"] },
+  { value: "twitter", label: "Twitter", icon: ["fab", "twitter"] },
+  { value: "reddit", label: "Reddit", icon: ["fab", "reddit"] },
+  { value: "github", label: "Github", icon: ["fab", "github"] },
 ];
 
 export const IDENTITY_TYPES: readonly SelectOption[] = [
-    { value: "interrep", label: "InterRep", icon: null },
-    { value: "random", label: "Random", icon: null },
+  { value: "interrep", label: "InterRep", icon: null },
+  { value: "random", label: "Random", icon: null },
 ];
 
 export { Paths } from "./paths";
